e6547efc17d5203f6af267a58ed209f93fe5e185
Alejandro Medina <drolejan@gmail.com>
2022-10-06 11:33:29 -0500

    Prueba Inputs Clase

--- Assets/controlTradicional.cs
+++ Assets/controlTradicional.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class controlTradicional : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public float playerSpeed;
+    PlayerInput misInputs;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        misInputs = GetComponent<PlayerInput>();
     }
-
-    // Update is called once per frame
+    public GameObject bala;
     void Update()
     {
-        
+        Vector2 movimiento = misInputs.actions["Movimiento"].ReadValue<Vector2>();
+        rb.velocity = movimiento*playerSpeed;
+
+        if (misInputs.actions["Disparo"].WasPressedThisFrame())
+        {
+            Instantiate(bala, transform.position, Quaternion.identity);
+        }
+      
     }
+
 }
